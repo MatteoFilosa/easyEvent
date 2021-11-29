faee1917462606c77daaf20272f9c0d7f0702765
MatteoFilosa <filosa.1793997@studenti.uniroma1.it>
2021-11-29 13:53:44 +0100

    .

---
+++ app/src/main/java/com/example/easyEvent/RegisterActivity.kt
@@ -0,0 +1,19 @@
+package com.example.easyEvent
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import android.widget.Toast
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        //register_button.setOnClickListener{}
+    }
+}
