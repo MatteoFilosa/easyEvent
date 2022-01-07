afa4d1e5d1cc265edf13b4cfd3d694c0b3fc9b2e
gf205 <ziscojr@gmail.com>
2022-01-07 13:50:14 +0100

    comp_hybrid initial commit

--- app/src/main/java/com/example/easyEvent/MainActivity.kt
+++ app/src/main/java/com/example/easyEvent/MainActivity.kt
@@ -4,6 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.dimensionResource
+import com.example.easyEvent.compose.EventCard
+import com.google.android.material.composethemeadapter.MdcTheme
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
@@ -14,20 +27,26 @@ class MainActivity : AppCompatActivity() {
         val userDisplayed = findViewById<TextView>(R.id.user)
         val editprofile_btn = findViewById<TextView>(R.id.editprofile_button)
 
-        val userId = intent.getStringExtra("user_id")
+        val mainComposeView = findViewById<ComposeView>(R.id.main_compose_view)
+
         val email = intent.getStringExtra("email_id")
+        val userId = intent.getStringExtra("user_id")
         userDisplayed.text = "email :: $email"
 
 
         editprofile_btn.setOnClickListener{
-
             val intent = Intent(this@MainActivity, ProfileInfosActivity::class.java)
             intent.flags =
                 Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(intent)
             finish()
         }
+        
+        mainComposeView.setContent {
+            MdcTheme {
+                EventCard("Test")
+            }
+        }
+
     }
-
-
 }
