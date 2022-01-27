2319b870e255d6dd3592076e80c6206c687b3bbc
gf205 <ziscojr@gmail.com>
2022-01-27 13:01:41 +0100

    rest with retrofit (error fixed)

--- app/src/main/java/com/example/easyEvent/EventAdapter.kt
+++ app/src/main/java/com/example/easyEvent/EventAdapter.kt
@@ -3,10 +3,12 @@ package com.example.easyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.easyEvent.R
 import androidx.recyclerview.widget.RecyclerView
 import com.example.easyEvent.databinding.HomeRvItemViewBinding
 import com.example.easyEvent.model.EventModel
+import androidx.appcompat.app.AppCompatActivity
 
 class EventAdapter: RecyclerView.Adapter<EventAdapter.EventViewHolder>() {
 
@@ -35,10 +37,10 @@ class EventAdapter: RecyclerView.Adapter<EventAdapter.EventViewHolder>() {
 
     class EventViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         fun bindView(item: EventModel?) {
-            var  tvHomeItemTitle = itemView.findViewById(R.id.tv_home_item_title)
-            var tvHomeItemBody = itemView.findViewById(R.id.tv_home_item_body)
-            itemView.tv_home_item_title.text = item?.title
-            itemView.tv_home_item_body.text = item?.description
+            val  tvHomeItemTitle = itemView.findViewById<TextView>(R.id.tv_home_item_title)
+            val tvHomeItemBody = itemView.findViewById<TextView>(R.id.tv_home_item_body)
+            tvHomeItemTitle.text = item?.title
+            tvHomeItemBody.text = item?.description
         }
     }
 }
--- app/src/main/java/com/example/easyEvent/network/ApiInterface.kt
+++ app/src/main/java/com/example/easyEvent/network/ApiInterface.kt
@@ -10,4 +10,6 @@ interface ApiInterface {
     @Headers("apikey: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MjA4MDk5MiwiZXhwIjoxOTU3NjU2OTkyfQ.8_5-1smAi3jcbnUJ40sVJ693PlA1kGD7EdONOvUpaBI")
     @GET("events")
     fun fetchAllEvents(): Call<List<EventModel>>
+
+
 }
