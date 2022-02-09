cc5e6423444b4c282a3a1f58e47420561cd0b8bd
MatteoFilosa <filosa.1793997@studenti.uniroma1.it>
2022-02-09 15:22:26 +0100

    fixed main

--- app/src/main/java/com/example/easyEvent/GetEventsActivity.kt
+++ app/src/main/java/com/example/easyEvent/GetEventsActivity.kt
@@ -1,15 +1,32 @@
 package com.example.easyEvent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class GetEventsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_event_list)
 
+        /* not working
+        val location = findViewById<TextView>(R.id.detail_location).toString()
+        val mapsBtn = findViewById<TextView>(R.id.fab_view_map)
+        Log.d("ECCOLO" ,location)
 
+        mapsBtn.setOnClickListener() {
+
+            val intent =
+                Intent(this@GetEventsActivity, MapsActivity::class.java)
+            intent.putExtra("location", location)
+            startActivity(intent)
+
+
+
+        }*/
+    }
 
 
     }
-}
